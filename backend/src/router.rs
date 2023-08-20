d9c4708d793c92c6e7f5abe8cb38d527adac5d78
mwombeki6 <hackerlubex@protonmail.com>
2023-08-20 19:54:40 +0300

    add login function

--- backend/src/router.rs
+++ backend/src/router.rs
@@ -6,13 +6,13 @@ pub struct LoginDetails {
 
 pub fn create_router(state: AppState, folder: PathBuf) -> Router {
     let api_router = Router::new()
-            .route("/register", post(register))
-            .route("/login", post(login))
-            .with_state(state);
+        .route("/register", post(register))
+        .route("/login", post(login))
+        .with_state(state);
 
     Router::new()
         .nest("/api", api_router)
-        .merge(SpaRouter::new("/", static_folder).index_file("index.html"))    
+        .merge(SpaRouter::new("/", static_folder).index_file("index.html"))
 }
 
 pub async fn register(
@@ -33,5 +33,42 @@ pub async fn register(
             format!("Something went wrong: {e}"),
         )
             .into_response(),
+    }
+}
+
+pub async fn login(
+    State(mut state): State<AppState>,
+    jar: PrivateCookiesJar,
+    Json(login): Json<LoginDetails>,
+) -> Result<(PrivateCookiesJar, StatusCode), StatusCode> {
+    let query = sqlx::query("SELECT * FROM users WHERE username = $1")
+        .bind(&login.username)
+        .fetch_optional(&state.postgres);
+
+    match query.await {
+        Ok(res) => {
+            if bcrypt::verify(login.password, res.unwrap().get("password")).is_err() {
+                return Err(StatusCode::BAD_REQUEST);
+            }
+            let session_id = rand::random::<u64>().to_string();
+
+            sqlx::query("INSERT INTO sessions (session_id, user_id) VALUES ($1, $2) ON CONFLICT (user_id) DO UPDATE SET session_id = EXCLUDED.session_id")
+                .bind(&session_id)
+                .bind(res.get::<i32, _>("id"))
+                .execute(&state.postgres)
+                .await
+                .expect("Couldn't insert session :(");
+
+            let cookie = Cookie::build("foo", session_id)
+                .secure(true)
+                .same_site(SameSite::Strict)
+                .http_only(true)
+                .path("/")
+                .finish();
+
+            Ok((jar.add(cookie), StatusCode::Ok))
+        }
+
+        Err(_) => Err(StatusCode::BAD_REQUEST),
     }
 }
