5cadbc622b4eade36e46a9c7aea450383e309972
mwombeki6 <hackerlubex@protonmail.com>
2023-08-20 21:44:44 +0300

    refactor create_router

--- backend/src/router.rs
+++ backend/src/router.rs
@@ -6,47 +6,46 @@ pub struct LoginDetails {
 
 pub fn api_router(state: AppState) -> Router {
     // CORS is required for our app to work
-        let cors = CorsLayer::new()
-            .allow_credentials(true)
-            .allow_methods(vec![Method::GET, Method::POST, Method::PUT, Method::DELETE])
-            .allow_headers(vec![ORIGIN, AUTHORIZATION, ACCEPT])
-            .allow_origin(state.domain.parse::<HeaderValue>().unwrap());
-    
-    // declare the records router
-        let notes_router = Router::new()
-            .route("/", get(view_records))
-            .route("/create", post(create_record))
-            .route(
-    // you can add multiple request methods to a route like this
-                "/:id",       get(view_one_record).put(edit_record).delete(destroy_record),
-            )
-            .route_layer(middleware::from_fn_with_state(
-                state.clone(),
-                validate_session,
-            ));
-    
-    // the routes in this router should be public, so no middleware is required
-        let auth_router = Router::new()
-            .route("/register", post(register))
-            .route("/login", post(login))
-            .route("/forgot", post(forgot_password))
-            .route("/logout", get(logout));
-    
-    // return router that uses all routes from both individual routers, but add the CORS layer as well as AppState which is defined in our entrypoint function
-        Router::new()
-            .route("/health", get(health_check))
-            .nest("/notes", notes_router)
-            .nest("/auth", auth_router)
-            .with_state(state)
-            .layer(cors)
-    }
+    let cors = CorsLayer::new()
+        .allow_credentials(true)
+        .allow_methods(vec![Method::GET, Method::POST, Method::PUT, Method::DELETE])
+        .allow_headers(vec![ORIGIN, AUTHORIZATION, ACCEPT])
+        .allow_origin(state.domain.parse::<HeaderValue>().unwrap());
 
-pub fn create_router(state: AppState, folder: PathBuf) -> Router {
-    let api_router = Router::new()
+    // declare the records router
+    let notes_router = Router::new()
+        .route("/", get(view_records))
+        .route("/create", post(create_record))
+        .route(
+            // you can add multiple request methods to a route like this
+            "/:id",
+            get(view_one_record).put(edit_record).delete(destroy_record),
+        )
+        .route_layer(middleware::from_fn_with_state(
+            state.clone(),
+            validate_session,
+        ));
+
+    // the routes in this router should be public, so no middleware is required
+    let auth_router = Router::new()
         .route("/register", post(register))
         .route("/login", post(login))
-        .with_state(state);
+        .route("/forgot", post(forgot_password))
+        .route("/logout", get(logout));
 
+    // return router that uses all routes from both individual routers, but add the CORS layer as well as AppState which is defined in our entrypoint function
+    Router::new()
+        .route("/health", get(health_check))
+        .nest("/notes", notes_router)
+        .nest("/auth", auth_router)
+        .with_state(state)
+        .layer(cors)
+}
+
+pub fn create_router(static_folder: PathBuf, state: AppState) -> Router {
+    let api_router = api_router(state);
+
+    // merge our static file assets
     Router::new()
         .nest("/api", api_router)
         .merge(SpaRouter::new("/", static_folder).index_file("index.html"))
@@ -134,13 +133,13 @@ pub async fn forgot_password(
 ) -> Response {
     let new_password = Alphanumeric.sample_string(&mut rand::thread_rng(), 16);
 
-let hashed_password = bcrypt::hash(&new_password, 10).unwrap();
+    let hashed_password = bcrypt::hash(&new_password, 10).unwrap();
 
     sqlx::query("UPDATE users SET password = $1 WHERE email = $2")
-            .bind(hashed_password)
-            .bind(email_recipient)
-            .execute(&state.postgres)
-            .await;
+        .bind(hashed_password)
+        .bind(email_recipient)
+        .execute(&state.postgres)
+        .await;
 
     let credentials = Credentials::new(state.smtp_email, state.smtp_password);
 
@@ -154,13 +153,13 @@ let hashed_password = bcrypt::hash(&new_password, 10).unwrap();
         .body(message)
         .unwrap();
 
-// build the SMTP relay with our credentials - in this case we'll be using gmail's SMTP because it's free
+    // build the SMTP relay with our credentials - in this case we'll be using gmail's SMTP because it's free
     let mailer = SmtpTransport::relay("smtp.gmail.com")
         .unwrap()
         .credentials(credentials)
         .build();
 
-// this part x`doesn't really matter since we don't want the user to explicitly know if they've actually received an email or not for security purposes, but if we do then we can create an output based on what we return to the client
+    // this part x`doesn't really matter since we don't want the user to explicitly know if they've actually received an email or not for security purposes, but if we do then we can create an output based on what we return to the client
     match mailer.send(&email) {
         Ok(_) => (StatusCode::OK, "Sent".to_string()).into_response(),
         Err(e) => (StatusCode::BAD_REQUEST, format!("Error: {e}")).into_response(),
@@ -170,27 +169,30 @@ let hashed_password = bcrypt::hash(&new_password, 10).unwrap();
 pub async fn validate_session<B>(
     jar: PrivateCookieJar,
     State(state): State<AppState>,
-// Request<B> and Next<B> are required types for middleware from a function in axum
+    // Request<B> and Next<B> are required types for middleware from a function in axum
     request: Request<B>,
     next: Next<B>,
 ) -> (PrivateCookieJar, Response) {
-// attempt to get the cookie - if it can't find a cookie, return 403
+    // attempt to get the cookie - if it can't find a cookie, return 403
     let Some(cookie) = jar.get("foo").map(|cookie| cookie.value().to_owned()) else {
 
         println!("Couldn't find a cookie in the jar");
         return (jar,(StatusCode::FORBIDDEN, "Forbidden!".to_string()).into_response())
     };
 
-// attempt to find the created session
+    // attempt to find the created session
     let find_session = sqlx::query("SELECT * FROM sessions WHERE session_id = $1")
-                .bind(cookie)
-                .execute(&state.postgres)
-                .await;
+        .bind(cookie)
+        .execute(&state.postgres)
+        .await;
 
-// if the created session is OK, carry on as normal and run the route - else, return 403
+    // if the created session is OK, carry on as normal and run the route - else, return 403
     match find_session {
         Ok(res) => (jar, next.run(request).await),
-        Err(_) => (jar, (StatusCode::FORBIDDEN, "Forbidden!".to_string()).into_response())
+        Err(_) => (
+            jar,
+            (StatusCode::FORBIDDEN, "Forbidden!".to_string()).into_response(),
+        ),
     }
 }
 
@@ -204,7 +206,8 @@ pub struct Note {
 pub async fn view_records(State(state): State<AppState>) -> Json<Vec<Note>> {
     let notes: Vec<Note> = sqlx::query_as("SELECT * FROM notes")
         .fetch_all(&state.postgres)
-        .await.unwrap();
+        .await
+        .unwrap();
 
     Json(notes)
 }
@@ -239,7 +242,6 @@ pub async fn edit_record(
     Path(id): Path<i32>,
     Json(request): Json<RecordRequest>,
 ) -> Response {
-    
     let query = sqlx::query("UPDATE notes SET message = $1 WHERE id = $2 AND owner = $3")
         .bind(request.message)
         .bind(id)
